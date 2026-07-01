00029205f53e1171514b83103099c6baa3abb810
MadsRThomsen <mrt@lifescience-robotics.com>
2026-07-01 13:06:32 +0200

    minimal working scraper

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
@@ -20,13 +21,10 @@ func main() {
 		log.Fatal("too many arguments provided")
 	}
 	baseURL := args[0]
+	pages := make(map[string]int)
 
-	html, err := getHTML(baseURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(html)
-
+	crawlPage(baseURL, baseURL, pages)
+	fmt.Printf("Pages scraped: %v\n", len(pages))
 }
 
 func getHTML(rawURL string) (string, error) {
@@ -58,4 +56,48 @@ func getHTML(rawURL string) (string, error) {
 	}
 
 	return string(body), nil
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		return
+	}
+
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return
+	}
+
+	if currentURL.Hostname() != baseURL.Hostname() {
+		return // Skips other websites
+	}
+
+	normCurrURL, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		return
+	}
+
+	// increment and return if page has been crawled, start at 1 if it has not
+	if _, v := pages[normCurrURL]; v {
+		pages[normCurrURL]++
+		return
+	}
+	pages[normCurrURL] = 1
+
+	fmt.Printf("Crawling page: %v\n", rawCurrentURL)
+	html, err := getHTML(rawCurrentURL)
+	if err != nil {
+		return
+	}
+
+	nextURLs, err := getURLsFromHTML(html, baseURL)
+	if err != nil {
+		return
+	}
+
+	for _, nextURL := range nextURLs {
+		crawlPage(rawBaseURL, nextURL, pages)
+	}
+
 }
